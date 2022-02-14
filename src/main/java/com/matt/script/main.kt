f6faaae8798632eac475e182595761538f2a25f6
wgyscsf <wgyscsf@163.com>
2022-02-14 14:15:17 +0800

    微调

--- src/main/java/com/matt/script/main.kt
+++ src/main/java/com/matt/script/main.kt
@@ -1,6 +1,18 @@
 import com.matt.script.AndroidScript
+import com.matt.script.core.KeyConvertCore
 
 fun main(args: Array<String>) {
+    /**
+     * 把项目中语言配置文件string.xml文件抽取成Excel
+     */
     AndroidScript.extractAndroidProject2Excel()
+    /**
+     * 将Excel回写成string.xml
+     */
     //AndroidScript.excel2StringsXml()
+
+    /**
+     * 项目中旧key替换成新key
+     */
+    KeyConvertCore.scanCore()
 }
