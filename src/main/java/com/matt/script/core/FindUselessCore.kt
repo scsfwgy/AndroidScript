8faa0a5b0b1dab01811eac8b03e9d09d75fa45a5
wgyscsf <wgyscsf@163.com>
2022-02-22 11:25:38 +0800

    整理逻辑，抽出公用逻辑

--- src/main/java/com/matt/script/core/FindUselessCore.kt
+++ src/main/java/com/matt/script/core/FindUselessCore.kt
@@ -15,68 +15,31 @@ fun main() {
 
 object FindUselessCore {
     fun findUselessStringXml() {
-        val dirPath = listOf(
-            "/Users/matt.wang/AsProject/Android-LBK/app/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/lib_wrapper/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/MPChartLib/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/mycommonlib/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/faceidmodule/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/lbankcorelib/src/main",
-        )
+        val dirPath = scanDirList()
         val stringXmlPath =
             "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/mycommonlib/src/main/res/values/strings.xml"
         val allExistSet = XmlCore.stringsXml2SortKeyList(stringXmlPath).toMutableSet()
-        val uselessSet = find(dirPath, allExistSet, object : FileFindKey {
-            override fun find(file: File): Set<String>? {
-                val readText = file.readText()
-                val splitFileByDot = FileUtilsWrapper.splitFileByDot(file)
-                return when (splitFileByDot.second) {
-                    "java", "kt" -> {
-                        RegexUtils.getMatches(RegexUtilsWrapper.javaOrKtPureStringKeyRegex, readText).toSet()
-                    }
-                    "xml" -> {
-                        RegexUtils.getMatches(RegexUtilsWrapper.xmlPureStringKeyRegex, readText).toSet()
-                    }
-                    else -> {
-                        null
-                    }
-                }
-            }
-        }, object : FileFilter {
-            override fun filter(file: File): Boolean {
-                val splitFileByDot = FileUtilsWrapper.splitFileByDot(file)
-                val second = splitFileByDot.second
-                return second == "java" || second == "kt" || second == "xml" || second == "m"
-            }
-        })
+        val uselessSet = findUselessSetWrapper(
+            dirPath,
+            allExistSet,
+            RegexUtilsWrapper.javaOrKtPureStringKeyRegex,
+            RegexUtilsWrapper.xmlPureStringKeyRegex
+        )
         LogUtils.loggerWrapper(FindUselessCore.javaClass).debug("未使用列表：" + uselessSet)
-        //后续怎么处理，自己定
     }
 
 
     fun findUselessDrawable() {
-        val dirPath = listOf(
-            "/Users/matt.wang/AsProject/Android-LBK/app/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/lib_wrapper/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/MPChartLib/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/mycommonlib/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/faceidmodule/src/main",
-            "/Users/matt.wang/AsProject/Android-LBK/lbankcorelib/src/main",
-        )
-
+        val dirPath = scanDirList()
         val realList = FileUtilsWrapper.getFilterDirPath(dirPath, "res", object : FileFilter {
             override fun filter(file: File): Boolean {
                 return file.isDirectory && file.name.startsWith("drawable")
             }
         }).map { it.path }
-
-        LogUtils.loggerWrapper(FindUselessCore.javaClass).debug(realList)
-
         val originKeyList = FileUtilsWrapper.scanDirList(realList, object : FileFilter {
             override fun filter(file: File): Boolean {
                 return true
             }
-
         })
         val allExistSet = originKeyList.map {
             //val splitFileByDot = FileUtilsWrapper.splitFileByDot(it)
@@ -85,19 +48,46 @@ object FindUselessCore {
             FileUtilsWrapper.getPureName(it)
         }.toMutableSet()
 
-        LogUtils.loggerWrapper(FindUselessCore.javaClass).debug(allExistSet.size.toString() + "===>>" + allExistSet)
-        LogUtils.loggerWrapper(FindUselessCore.javaClass).debug(allExistSet.size.toString())
+        val uselessSet = findUselessSetWrapper(
+            dirPath,
+            allExistSet,
+            RegexUtilsWrapper.javaOrKtPureDrawableKeyRegex,
+            RegexUtilsWrapper.xmlPureDrawableKeyRegex
+        )
+        LogUtils.loggerWrapper(FindUselessCore.javaClass).debug("未使用列表：" + uselessSet)
+        //后续怎么处理，自己定
+        val delFileList = originKeyList.filter {
+            //不取后缀
+            val splitFileByDot = FileUtilsWrapper.getPureName(it)
+            uselessSet.contains(splitFileByDot)
+        }
+        LogUtils.loggerWrapper(FindUselessCore.javaClass)
+            .debug("待删除文件：" + delFileList.size.toString() + "===>>" + delFileList)
+        val sum = delFileList.sumOf { it.length() }
+        LogUtils.loggerWrapper(FindUselessCore.javaClass)
+            .debug("待删除文件大小（总计）：" + FileUtils.byte2FitMemorySize(sum, 2))
 
-        val uselessSet = find(dirPath, allExistSet, object : FileFindKey {
+//        delFileList.forEach {
+//            it.deleteOnExit()
+//        }
+    }
+
+    fun findUselessSetWrapper(
+        dirPath: List<String>,
+        allExistSet: Set<String>,
+        javaOrKtRegex: String,
+        xmlRegex: String,
+    ): Set<String> {
+        val uselessSet = findUselessSet(dirPath, allExistSet, object : FileFindKey {
             override fun find(file: File): Set<String>? {
                 val readText = file.readText()
                 val splitFileByDot = FileUtilsWrapper.splitFileByDot(file)
                 return when (splitFileByDot.second) {
                     "java", "kt" -> {
-                        RegexUtils.getMatches(RegexUtilsWrapper.javaOrKtPureDrawableKeyRegex, readText).toSet()
+                        RegexUtils.getMatches(javaOrKtRegex, readText).toSet()
                     }
                     "xml" -> {
-                        RegexUtils.getMatches(RegexUtilsWrapper.xmlPureDrawableKeyRegex, readText).toSet()
+                        RegexUtils.getMatches(xmlRegex, readText).toSet()
                     }
                     else -> {
                         null
@@ -113,24 +103,10 @@ object FindUselessCore {
         })
         LogUtils.loggerWrapper(FindUselessCore.javaClass)
             .debug("未使用列表：" + uselessSet.size.toString() + "===>>" + uselessSet)
-        //后续怎么处理，自己定
-        val delFileList = originKeyList.filter {
-            //不取后缀
-            val splitFileByDot = FileUtilsWrapper.getPureName(it)
-            uselessSet.contains(splitFileByDot)
-        }
-        LogUtils.loggerWrapper(FindUselessCore.javaClass)
-            .debug("待删除文件：" + delFileList.size.toString() + "===>>" + delFileList)
-        val sum = delFileList.sumOf { it.length() }
-        LogUtils.loggerWrapper(FindUselessCore.javaClass)
-            .debug("待删除文件大小（总计）：" + FileUtils.byte2FitMemorySize(sum, 2))
-//        delFileList.forEach {
-//            it.deleteOnExit()
-//        }
+        return uselessSet
     }
 
-
-    fun find(
+    fun findUselessSet(
         dirPath: List<String>,
         allExistSet: Set<String>,
         fileFindKey: FileFindKey,
@@ -165,5 +141,16 @@ object FindUselessCore {
                 //LogUtils.loggerWrapper(FindUselessCore.javaClass).debug("==>" + it)
             }
         }
+    }
+
+    fun scanDirList(): List<String> {
+        return listOf(
+            "/Users/matt.wang/AsProject/Android-LBK/app/src/main",
+            "/Users/matt.wang/AsProject/Android-LBK/lib_wrapper/src/main",
+            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/MPChartLib/src/main",
+            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/mycommonlib/src/main",
+            "/Users/matt.wang/AsProject/Android-LBK/third_part_lib/faceidmodule/src/main",
+            "/Users/matt.wang/AsProject/Android-LBK/lbankcorelib/src/main",
+        )
     }
 }
